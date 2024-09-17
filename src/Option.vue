fae4baf1fb16a869b5ca549f85d8caf8c7491d36
San Vert <itsanvert@gmail.com>
2024-09-17 20:16:47 +0700

    D1

---
+++ src/Option.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+data() {
+    return {
+      name : "Jonh Doe",
+      status : 'active',
+      tasks : ['Task One', 'Task Two', 'Task Three'],
+      link : 'https://google.com',
+    };
+  },
+  methods: {
+    toggleStatus () {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = 'inactive';
+      }  else {
+        this.status = 'active'
+      }
+    },
+  },
+};
+</script>
